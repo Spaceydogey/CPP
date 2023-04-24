bdd2c876756980d7df9b9cfbb3feab8cb707c12b
Hugo Delmas <hdelmas@fu-r1-p7.s19.be>
2023-04-24 11:03:34 +0200

    fixed harlfilter

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:58:44 by hdelmas           #+#    #+#             */
-/*   Updated: 2023/04/20 19:50:47 by hdelmas          ###   ########.fr       */
+/*   Updated: 2023/04/24 11:03:13 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 int	main(int ac, char **av)
 {
 	Harl	harl;
+	
 	if (ac != 2)
 	{
 		std::cerr << "Error: Wrong number of arguments" << std::endl;
+		return (1);
 	}
 	harl.complain(av[1]);
 }
