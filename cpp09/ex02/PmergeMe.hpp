2af4a5534560e23befb0f1f5e556ab2f259e9d16
Delmas Hugo <delmas.hugo@gmail.com>
2023-06-06 14:03:44 +0200

    merge done

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:46:25 by hdelmas           #+#    #+#             */
-/*   Updated: 2023/06/05 23:17:25 by hdelmas          ###   ########.fr       */
+/*   Updated: 2023/06/06 14:03:17 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,21 +43,53 @@ T	&makePairs(T &container) //we assume that container size is even
 	}
 	return (container);
 }
+template <typename T>
+T	mergePairs(T &lhs, T &rhs) //we assume that container size is even
+{
+	T			res;
+	typename T::iterator	max_lhs;
+	typename T::iterator	max_rhs;
+	
+	while (!(lhs.empty() && rhs.empty()))
+	{
+		max_lhs = std::max_element(lhs.begin(), lhs.end());
+		max_rhs = std::max_element(rhs.begin(), rhs.end());
+		if (rhs.empty() || (*max_lhs > *max_rhs && !lhs.empty() ))
+		{
+			res.insert(res.begin(), max_lhs - 1, max_lhs + 1);
+			lhs.erase(max_lhs - 1, max_lhs + 1);	
+		}
+		else
+		{
+			res.insert(res.begin(), max_rhs - 1, max_rhs + 1);	
+			rhs.erase(max_rhs - 1, max_rhs + 1);	
+		}
+	}
+	return (res);
+}
 
 template <typename T>
 T	&orderPairs(T &container) //we assume that container size is even
 {
 	// We need to mergesort container on the 2n + 1 values so we have a container ordered by pairs size every 2n+1 indexes are the max value of a pair
-	//division of container for the merge sort
-	int split = (container.size() / 2) - 1;
-	if (split % 2 == 0)
-		--split;
-	T	lhs(container.begin(), container.begin() + split + 1);
-	T	rhs(container.begin() + split + 1, container.end());
-	std::cout << "lhs = ";
-	ft_print(lhs);
-	std::cout << "rhs = ";
-	ft_print(rhs);
+	int size = container.size();
+	if (size > 2) // division of container for the merge sort
+	{
+		int split = (size / 2);
+		if (split % 2 == 1)		//if Split is odd we need to take into account that we want to merge sort with pairs
+			--split;
+			
+		// Split into 2 T
+		T	lhs(container.begin(), container.begin() + split);
+		T	rhs(container.begin() + split, container.end());
+		
+		// Recursion to merge sort each split
+		lhs = orderPairs(lhs);
+		rhs = orderPairs(rhs);
+		
+		// Merge
+		container = mergePairs(lhs, rhs);
+	}
 	return (container);
 }
 #endif
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,14 +6,12 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 15:08:43 by hdelmas           #+#    #+#             */
-/*   Updated: 2023/06/05 23:19:52 by hdelmas          ###   ########.fr       */
+/*   Updated: 2023/06/06 14:03:21 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 # include <vector>
-
-
 
 int	main(int ac, char** av)
 {
@@ -22,8 +20,16 @@ int	main(int ac, char** av)
 	std::vector<int> v;
 	for (int i = 0; i < 6; ++i)
 		v.push_back(i);
+	v[0] = 11;
+	v[1] = 10;
+	v[2] = 9;
+	v[3] = 6;
+	v[4] = 8;
+	v[5] = 1;
 	ft_print(v);
 	makePairs(v);
 	ft_print(v);
 	orderPairs(v);
+	ft_print(v);
+
 }
